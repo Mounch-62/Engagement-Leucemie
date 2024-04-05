4779fe8367da30e24896dae5638e480b1ea162b9
Louis <louisbailly.1234@gmail.com>
2024-04-05 11:07:34 +0200

    fix : web.php

--- routes/web.php
+++ routes/web.php
@@ -26,24 +26,18 @@ Route::post('/el-admin/bureau/add', [BureauController::class, 'store'])->name('a
 Route::put('/el-admin/bureau/update/{id}', [BureauController::class, 'update'])->name('admin.bureau.update');
 Route::delete('/el-admin/bureau/delete/{id}', [BureauController::class, 'destroy'])->name('admin.bureau.destroy');
 
-Route::get('/', function () {
-    return view('base.home');
-});
+
 Route::get('/el-admin', function () {
     return view('admin.base-admin');
 });
 Route::get('/el-admin/bureau', [BureauController::class, 'index'])->name('admin.bureau');
 Route::resource('/el-admin/actualite', ActualiteController::class);
-Route::get('/front', [FrontController::class, 'home'])->name("front.home");
+
 Route::get('/actualite', [FrontController::class, 'actualite'])->name("front.actualite");
 Route::get('/ajouterActu', [ActualiteController::class, 'create'])->name("ajouter_actu");
 Route::post('/traiter_form', [ActualiteController::class, 'store'])->name("enreg_actu");
 Route::delete('/delete/{id}', [ActualiteController::class, 'destroy'])->name('delete_actualite')->where('id', '[0-9]+');
 
-Route::get('/carousel', [MediaController::class, 'image'])->name("front.carousel");
 
-
-
-Route::get('/videos', [MediaController::class, 'video'])->name('front.video');
 Route::get('/front/bandeauInscrit',[FrontController::class,'test'])->name('front.bandeau-inscrit');
-Route::get('/el-admin/actualite', [MediaController::class,'backActualite'])->name('admin.actualite.index');
+Route::get('/el-admin/actualite', [ActualiteController::class,'index'])->name('admin.actualite.index');
